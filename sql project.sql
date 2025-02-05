8dcc19752f8223d554281985d9b42b4c6e900229
Talha Türkoğlu <158575414+talhaturkoqlu61@users.noreply.github.com>
2025-02-05 18:35:36 +0300

    SQLproject1

---
+++ sql project.sql
@@ -0,0 +1,9 @@
+-- SELECT title,description FROM film;(1.quesiton)
+--SELECT * FROM film
+--Where length >60 AND length <75;(2.question)
+--SELECT * FROM film
+--WHERE rental_rate=0.99 AND (replacement_cost=12.99 OR replacement_cost=28.99);(3.question)
+--SELECT last_name FROM customer
+--Where first_name='Mary';(4.question)
+--SELECT * FROM film
+--WHERE length<=50 AND rental_rate!=2.99 AND rental_rate!=4.99;(5.question)
